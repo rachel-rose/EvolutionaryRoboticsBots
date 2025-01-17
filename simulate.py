705eb77c116540f44f7d4cb26fab36bf2894b365
rachel-rose <rachel.rose@uvm.edu>
2025-01-17 17:03:29 -0500

    winow stays open longer now

--- simulate.py
+++ simulate.py
@@ -1,3 +1,8 @@
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
+for i in range(1000):
+    p.stepSimulation()
+    print(f"interation num: {i}")
+    time.sleep(1/60)
 p.disconnect()

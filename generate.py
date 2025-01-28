3f652a9fb24efb6daba0fac0e7a4f0e2f0ef8be0
rachel-rose <rachel.rose@uvm.edu>
2025-01-27 20:28:48 -0500

    on step 22 of manylinks

--- generate.py
+++ generate.py
@@ -1,1 +1,10 @@
 import pyrosim.pyrosim as pyrosim
+pyrosim.Start_SDF("box.sdf")
+length = 1
+width = 1
+height = 3
+x= 0
+y = 0
+z = 0
+pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+pyrosim.End()

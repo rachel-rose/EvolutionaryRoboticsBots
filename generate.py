dc186a420089633ce693fa4a17ef4d99a4b751c4
rachel-rose <rachel.rose@uvm.edu>
2025-02-03 22:58:55 -0500

    three tower linked bot

--- generate.py
+++ generate.py
@@ -48,7 +48,9 @@ def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Link0", pos=[0, 0, 0.5] , size=[1, 1, 1])
     pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0.5, 0, 1])
-    pyrosim.Send_Cube(name="Link1", pos=[0.5,0,0.5] , size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Link1", pos=[-0.5,0,0.5] , size=[1, 1, 1])
+    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0, 0, 1])
+    pyrosim.Send_Cube(name="Link2", pos=[-0.5,0,0.5] , size=[1, 1, 1])
     pyrosim.End()
 
 # Modify generate.py to reflect this slide: rename the two links to Link0 and Link1, and rename the joint connecting them to Link0_Link1.

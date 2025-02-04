0d5d3eca28ef75dcbf1066177dbb69b1f19ec6ce
rachel-rose <rachel.rose@uvm.edu>
2025-02-03 22:31:43 -0500

    two blue boxes. Step 27 joints

--- generate.py
+++ generate.py
@@ -41,13 +41,16 @@ for i in range(num_cols):
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Box", pos=[5, 0, 0.5] , size=[1, 1, 1])
     pyrosim.End()
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5, 0, 1])
+    pyrosim.Send_Cube(name="Leg", pos=[1.0,0,1.5] , size=[1, 1, 1])
     pyrosim.End()
+
 
 Create_World()
 Create_Robot()

523d5b801165aeeff66d9511005f1dc09f4af8c9
rachel-rose <rachel.rose@uvm.edu>
2025-02-03 22:49:26 -0500

    diagonol blue boxes attached

--- generate.py
+++ generate.py
@@ -46,10 +46,12 @@ def Create_World():
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5] , size=[1, 1, 1])
-    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5, 0, 1])
-    pyrosim.Send_Cube(name="Leg", pos=[1.0,0,1.5] , size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Link0", pos=[0, 0, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0.5, 0, 1])
+    pyrosim.Send_Cube(name="Link1", pos=[0.5,0,0.5] , size=[1, 1, 1])
     pyrosim.End()
+
+# Modify generate.py to reflect this slide: rename the two links to Link0 and Link1, and rename the joint connecting them to Link0_Link1.
 
 
 Create_World()

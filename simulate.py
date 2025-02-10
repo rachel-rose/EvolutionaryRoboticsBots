9b327e1c71ec4e9688e6480811b24dbc02e32c0b
rachel-rose <rachel.rose@uvm.edu>
2025-02-10 11:56:17 -0500

    step 5

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet_data
 import pybullet as p
 import time
+import pyrosim.pyrosim as pyrosim
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -18,4 +19,7 @@ for i in range(4000):
     print(f"interation num: {i}")
     time.sleep(1/60)
 
+backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
 p.disconnect()
+
